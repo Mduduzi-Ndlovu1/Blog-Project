b76f01c661b9511e7ad8a92d105b38da0e7a14f6
Mduduzindlovu <Mduduzindlovu61@gmail.com>
2023-12-14 12:16:07 +0200

    npx nodemon on start and dev

--- server/config/db.js
+++ server/config/db.js
@@ -4,9 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+      useUnifiedTopology: true    });
     console.log('MongoDB Connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
