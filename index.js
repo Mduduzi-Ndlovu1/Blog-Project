bcda4311768844c5533ce7b4303622ba289f1ebe
Mduduzindlovu <Mduduzindlovu61@gmail.com>
2023-12-14 12:08:51 +0200

    okay chatgpt tried so lets see

--- index.js
+++ index.js
@@ -1,51 +1,48 @@
 require('dotenv').config();
-
-const express = require ('express');
-const expressLayout = require('express-ejs-layouts');
-const methodOveride = require('method-override');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const MongoStore = require('connect-mongo');
 const session = require('express-session');
-const { isActiveRoute} = require('./server/helpers/routeHelpers')
-
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
 const connectDB = require('./server/config/db');
 
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-// connect to db
+// Connect to the database
 connectDB();
 
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(methodOveride('_method'));
+app.use(methodOverride('_method'));
 
-app.use(session({
-    secret: 'keyboar cat',
+app.use(
+  session({
+    secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
-        mongoUrl:process.env.MONGODB_URL
-
+      mongoUrl: process.env.MONGODB_URL,
+      ttl: 7 * 24 * 60 * 60, // Session TTL (optional, adjust as needed)
     }),
-}));
+  })
+);
 
 app.use(express.static('public'));
 
-//templating engine
-app.use(expressLayout);
+// Templating engine
+app.use(expressLayouts);
 app.set('layout', './layouts/main');
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 app.locals.isActiveRoute = isActiveRoute;
 
-app.use('/',require('./server/routes/main'));
-app.use('/',require('./server/routes/admin'));
-
-
+app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-})
+  console.log(`App listening on port ${PORT}`);
+});
--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
-    try {
-        mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URL);
-        console.log(`Database Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
 
 module.exports = connectDB;
