74c5224be511cfe16d07ef99eadc2931b5be5e84
Mduduzindlovu <Mduduzindlovu61@gmail.com>
2023-12-14 12:19:55 +0200

    reset to OG

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,12 @@
-const mongoose = require('mongoose');
-
+const mongoose =require('mongoose');
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true    });
-    console.log('MongoDB Connected');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1); // Exit the process with a failure code
-  }
-};
+    try {
+        mongoose.set('strictQuery', false);
+        const conn = await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`Database Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 module.exports = connectDB;
